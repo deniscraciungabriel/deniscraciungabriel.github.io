4bffda00a5b11fec31c3ae7d42318fe7a3c90377
Denis Craciun <51826214+deniscraciungabriel@users.noreply.github.com>
2021-05-29 15:37:49 +0200

    Add files via upload

---
+++ about/about.js
@@ -0,0 +1,34 @@
+let selectedValue = 1;
+
+function SelectPrice() {
+    const rbs = document.querySelectorAll('input[name="price"]');
+    for (const rb of rbs) {
+        if (rb.checked) {
+            selectedValue = rb.value;
+            break;
+        }
+    }
+}
+console.log(selectedValue)
+paypal.Buttons({
+    // Set up the transaction
+    createOrder: function (data, actions) {
+        return actions.order.create({
+            purchase_units: [{
+                amount: {
+                    value: selectedValue
+                }
+            }]
+        });
+    },
+
+    // Finalize the transaction
+    onApprove: function (data, actions) {
+        return actions.order.capture().then(function (details) {
+            // Show a success message to the buyer
+            alert('Transaction completed by ' + details.payer.name.given_name + '!');
+        });
+    }
+
+
+}).render('#smart-button-container');
